42c8a2739b8738295acfec4e85ee1e9c1d31aa73
Steven Haddox <steven@haddox.us>
2012-07-19 23:20:19 -0400

    Update FactoryGirl Syntax

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,7 +4,7 @@ describe Link do
 
   describe 'Validations' do
     it "should be valid with valid attributes" do
-      link = Factory.build(:link)
+      link = FactoryGirl.build(:link)
       link.valid? # invoke before_validation hook
       link.should be_valid
       link.save
@@ -12,20 +12,20 @@ describe Link do
     end
     
     it "should not be valid without a long URL" do
-      link = Factory.build(:link, :original => nil)
+      link = FactoryGirl.build(:link, :original => nil)
       link.should_not be_valid
       link.should have(2).errors_on(:original)
     end
     
     it "should not be valid unless the long URL has valid syntax" do
-      link = Factory.build(:link, :original => 'Google')
+      link = FactoryGirl.build(:link, :original => 'Google')
       link.should_not be_valid
       link.should have(1).error_on(:original)
     end
    
     it "should not allow duplicate long URLs" do
-      link1 = Factory(:link, :original => 'http://google.com/?q=1')
-      link2 = Factory.build(:link, :original => 'http://google.com/?q=1')
+      link1 = FactoryGirl.create(:link, :original => 'http://google.com/?q=1')
+      link2 = FactoryGirl.build(:link, :original => 'http://google.com/?q=1')
       link2.original.should == link1.original
       link2.should_not be_valid
       link2.should have(1).error_on(:original)
@@ -35,7 +35,7 @@ describe Link do
   describe ".shorten" do
     
     it "should convert the original URL into a short URL before validating" do
-      link = Factory.build(:link)
+      link = FactoryGirl.build(:link)
       link.short.should be_nil
       link.valid?
       link.short.should_not be_nil
@@ -45,7 +45,7 @@ describe Link do
 
   describe ".short_url" do
     it "should return a full short URL for use in views" do
-      link = Factory(:link)
+      link = FactoryGirl.create(:link)
       link.short_url.should_not be_nil
       link.short_url.should include 'http'
     end
